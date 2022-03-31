76c05cd647617eb178f659ec1ce4527f911fd397
Justis Corbett <corbettjustis@gmail.com>
2022-03-31 16:34:26 -0600

    fixed imports, classNames

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import * as Tone from 'tone'
 import './App.css';
-import {useState} from 'react'
+import React, { useState } from "react";
 
 function App() {
     const [synth, setSynth] = useState(new Tone.Synth().toDestination());
@@ -22,90 +22,102 @@ function App() {
     const releaseNote = (key) => {
         synth.triggerRelease([key.dataset.note], undefined)
     }
-
-    let keyboard = document.getElementById("keyboard");
-    for(let key of keyboard.children){
-        key.addEventListener("mouseover",  function(){if(mouse_IsDown) playNote(key)});
-        key.addEventListener("mousedown",  function(){playNote(key)                 });
-        key.addEventListener("touchstart", function(){playNote(key)                 });
-        key.addEventListener("mouseleave", function(){releaseNote(key)              });
-        key.addEventListener("mouseup",    function(){releaseNote(key)              });
-        key.addEventListener("touchend",   function(){releaseNote(key)              });
-    }
+    React.useEffect(() => {
+        let keyboard = document.getElementById("keyboard");
+        for(let key of keyboard.children){
+            key.addEventListener("mouseover",  function(){if(mouse_IsDown) playNote(key)});
+            key.addEventListener("mousedown",  function(){playNote(key)                 });
+            key.addEventListener("touchstart", function(){playNote(key)                 });
+            key.addEventListener("mouseleave", function(){releaseNote(key)              });
+            key.addEventListener("mouseup",    function(){releaseNote(key)              });
+            key.addEventListener("touchend",   function(){releaseNote(key)              });
+        }
+        return () => {
+            let keyboard = document.getElementById("keyboard");
+            for(let key of keyboard.children){
+                key.removeEventListener("mouseover",  function(){if(mouse_IsDown) playNote(key)});
+                key.removeEventListener("mousedown",  function(){playNote(key)                 });
+                key.removeEventListener("touchstart", function(){playNote(key)                 });
+                key.removeEventListener("mouseleave", function(){releaseNote(key)              });
+                key.removeEventListener("mouseup",    function(){releaseNote(key)              });
+                key.removeEventListener("touchend",   function(){releaseNote(key)              });
+            }
+        }
+    }, [])
     return (
         <div className="App">
             <div id="keyboard">
-                <div class="key white c" data-note="C3">
+                <div className="key white c" data-note="C3">
                 </div>
-                <div class="key black c_sharp" data-note="C#3">
+                <div className="key black c_sharp" data-note="C#3">
                 </div>
-                <div class="key white d" data-note="D3">
+                <div className="key white d" data-note="D3">
                 </div>
-                <div class="key black d_sharp" data-note="D#3">
+                <div className="key black d_sharp" data-note="D#3">
                 </div>
-                <div class="key white e" data-note="E3">
+                <div className="key white e" data-note="E3">
                 </div>
-                <div class="key white f" data-note="F3">
+                <div className="key white f" data-note="F3">
                 </div>
-                <div class="key black f_sharp" data-note="F#3">
+                <div className="key black f_sharp" data-note="F#3">
                 </div>
-                <div class="key white g" data-note="G3">
+                <div className="key white g" data-note="G3">
                 </div>
-                <div class="key black g_sharp" data-note="G#3">
+                <div className="key black g_sharp" data-note="G#3">
                 </div>
-                <div class="key white a" data-note="A3">
+                <div className="key white a" data-note="A3">
                 </div>
-                <div class="key black a_sharp" data-note="A#3">
+                <div className="key black a_sharp" data-note="A#3">
                 </div>
-                <div class="key white b" data-note="B3">
+                <div className="key white b" data-note="B3">
                 </div>
-                <div class="key white c" data-note="C4">
+                <div className="key white c" data-note="C4">
                 </div>
-                <div class="key black c_sharp" data-note="C#4">
+                <div className="key black c_sharp" data-note="C#4">
                 </div>
-                <div class="key white d" data-note="D4">
+                <div className="key white d" data-note="D4">
                 </div>
-                <div class="key black d_sharp" data-note="D#4">
+                <div className="key black d_sharp" data-note="D#4">
                 </div>
-                <div class="key white e" data-note="E4">
+                <div className="key white e" data-note="E4">
                 </div>
-                <div class="key white f" data-note="F4">
+                <div className="key white f" data-note="F4">
                 </div>
-                <div class="key black f_sharp" data-note="F#4">
+                <div className="key black f_sharp" data-note="F#4">
                 </div>
-                <div class="key white g" data-note="G4">
+                <div className="key white g" data-note="G4">
                 </div>
-                <div class="key black g_sharp" data-note="G#4">
+                <div className="key black g_sharp" data-note="G#4">
                 </div>
-                <div class="key white a" data-note="A4">
+                <div className="key white a" data-note="A4">
                 </div>
-                <div class="key black a_sharp" data-note="A#4">
+                <div className="key black a_sharp" data-note="A#4">
                 </div>
-                <div class="key white b" data-note="B4">
+                <div className="key white b" data-note="B4">
                 </div>
-                <div class="key white c" data-note="C5">
+                <div className="key white c" data-note="C5">
                 </div>
-                <div class="key black c_sharp" data-note="C#5">
+                <div className="key black c_sharp" data-note="C#5">
                 </div>
-                <div class="key white d" data-note="D5">
+                <div className="key white d" data-note="D5">
                 </div>
-                <div class="key black d_sharp" data-note="D#5">
+                <div className="key black d_sharp" data-note="D#5">
                 </div>
-                <div class="key white e" data-note="E5">
+                <div className="key white e" data-note="E5">
                 </div>
-                <div class="key white f" data-note="F5">
+                <div className="key white f" data-note="F5">
                 </div>
-                <div class="key black f_sharp" data-note="F#5">
+                <div className="key black f_sharp" data-note="F#5">
                 </div>
-                <div class="key white g" data-note="G5">
+                <div className="key white g" data-note="G5">
                 </div>
-                <div class="key black g_sharp" data-note="G#5">
+                <div className="key black g_sharp" data-note="G#5">
                 </div>
-                <div class="key white a" data-note="A5">
+                <div className="key white a" data-note="A5">
                 </div>
-                <div class="key black a_sharp" data-note="A#5">
+                <div className="key black a_sharp" data-note="A#5">
                 </div>
-                <div class="key white b" data-note="B5">
+                <div className="key white b" data-note="B5">
                 </div>
             </div>
         </div>
