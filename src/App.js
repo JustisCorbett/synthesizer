0ecdbe54e98f8c0dabbe970a60ced5d09d16d12a
Justis Corbett <corbettjustis@gmail.com>
2022-03-31 15:34:44 -0600

    synth state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import * as Tone from 'tone'
 import './App.css';
+import {useState} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [polySynth, setPolySynth] = useState(new Tone.PolySynth());
+    const [osc, setOsc] = useState(new Tone.OmniOscillator());
+    const [envelope, setEnvelope] = useState(new Tone.AmplitudeEnvelope());
+    const [filter, setFilter] = useState(new Tone.Filter());
+    const [reverb, setReverb] = useState(new Tone.Reverb());
+    const [delay, setDelay] = useState(new Tone.FeedbackDelay());
+    const [chorus, setChorus] = useState(new Tone.Chorus());
+    const [octave, setOctave] = useState(3);
+    return (
+        <div className="App">
+      
+        </div>
+    );
 }
 
 export default App;
