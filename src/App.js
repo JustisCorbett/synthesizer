fa4cbc9f1f65028fee08a906234992c54c5377c0
Justis Corbett <corbettjustis@gmail.com>
2022-04-25 17:59:01 -0600

    octave switch, trying to fix state bug

--- src/App.js
+++ src/App.js
@@ -20,16 +20,17 @@ function App() {
         modulation: {type: 'square'}, 
         modulationEnvelope: {attack: 0.5, decay: 0.0, sustain: 1, release: 0.5}
         });
+    const [octave, setOctave] = React.useState(0);
     // const [filter, setFilter] = useState(new Tone.Filter());
     // const [reverb, setReverb] = useState(new Tone.Reverb());
     // const [delay, setDelay] = useState(new Tone.FeedbackDelay());
     // const [chorus, setChorus] = useState(new Tone.Chorus());
-    const [octave, setOctave] = React.useState(3);
 
     console.log(polySynth);
     // TODO: visualize waveform
     let waveForm = new Tone.Waveform().toDestination();
     let mouseIsDown = false;
+    let octave1 = octave;
 
 
     const handleOscChange = (e) => {
@@ -41,6 +42,27 @@ function App() {
                 type: wave
                 }
             });
+    }
+
+    const handleOctaveChange = (e) => {
+        const selection = e.target.value;
+        switch (selection) {
+            case "0":
+                setOctave(octave => {octave = 0; return octave});
+                break;
+            case "1":
+                setOctave(octave => {octave = 3; return octave});
+                break;
+            case "2":
+                setOctave(octave => {octave = 5; return octave});
+                break;
+            case "3":
+                setOctave(octave => {octave = 7; return octave});
+                break;
+            default :
+                setOctave(octave => 0);
+        };
+        console.log(octave);
     }
 
     const handleMouseDown = (bool) => {
@@ -55,9 +77,12 @@ function App() {
     }
 
     const playNote = (key) => {
-        console.log(key.dataset.note);
+        console.log(octave1 + "state");
+        console.log(parseInt(key.dataset.octave));
+        console.log(key.dataset.note + (octave + parseInt(key.dataset.octave)).toString());
+        let note = key.dataset.note + (octave + parseInt(key.dataset.octave)).toString();
         Tone.start();
-        polySynth.triggerAttack(key.dataset.note, Tone.now());
+        polySynth.triggerAttack(note, Tone.now());
         if (key.classList.contains("white")) {
             key.classList.add("white-highlighted");
         } else {
@@ -66,9 +91,10 @@ function App() {
     }
 
     const releaseNote = (key) => {
-        polySynth.triggerRelease(key.dataset.note, Tone.now());
+        let note = key.dataset.note + (octave + parseInt(key.dataset.octave)).toString();
+        polySynth.triggerRelease(note, Tone.now());
         // triggerRelease twice to fix bug relating to triggering release on keydown.
-        polySynth.triggerRelease(key.dataset.note, Tone.now());
+        polySynth.triggerRelease(note, Tone.now());
         if (key.classList.contains("white")) {
             key.classList.remove("white-highlighted");
         } else {
@@ -135,83 +161,85 @@ function App() {
                             <div>TRI</div>
                         </div>
                         <div className="control-row">
-                            <input id="osc2-gain" type="range" value="0"/>
-                            <label htmlFor="osc2-gain">OSC 2</label>
+                            <input onChange={handleOctaveChange} id="octave" type="range" min="0" max="3"/>
+                        </div>
+                        <div className="control-row">
+                            <div>Octave</div>
                         </div>
                     </div>
                 </div>
                 <div id="keyboard">
-                    <div className="key white c" data-note="C3" data-key="z">
+                    <div className="key white c" data-note="C" data-octave="0" data-key="z">
                     </div>
-                    <div className="key black c_sharp" data-note="C#3" data-key="s">
+                    <div className="key black c_sharp" data-note="C#" data-octave="0" data-key="s">
                     </div>
-                    <div className="key white d" data-note="D3" data-key="x">
+                    <div className="key white d" data-note="D" data-octave="0" data-key="x">
                     </div>
-                    <div className="key black d_sharp" data-note="D#3" data-key="d">
+                    <div className="key black d_sharp" data-note="D#" data-octave="0" data-key="d">
                     </div>
-                    <div className="key white e" data-note="E3" data-key="c">
+                    <div className="key white e" data-note="E" data-octave="0" data-key="c">
                     </div>
-                    <div className="key white f" data-note="F3" data-key="v">
+                    <div className="key white f" data-note="F" data-octave="0" data-key="v">
                     </div>
-                    <div className="key black f_sharp" data-note="F#3" data-key="g">
+                    <div className="key black f_sharp" data-note="F#" data-octave="0" data-key="g">
                     </div>
-                    <div className="key white g" data-note="G3" data-key="b">
+                    <div className="key white g" data-note="G" data-octave="0" data-key="b">
                     </div>
-                    <div className="key black g_sharp" data-note="G#3" data-key="h">
+                    <div className="key black g_sharp" data-note="G#" data-octave="0" data-key="h">
                     </div>
-                    <div className="key white a" data-note="A3" data-key="n">
+                    <div className="key white a" data-note="A" data-octave="0" data-key="n">
                     </div>
-                    <div className="key black a_sharp" data-note="A#3" data-key="j">
+                    <div className="key black a_sharp" data-note="A#" data-octave="0" data-key="j">
                     </div>
-                    <div className="key white b" data-note="B3" data-key="m">
+                    <div className="key white b" data-note="B" data-octave="0" data-key="m">
                     </div>
-                    <div className="key white c" data-note="C4" data-key=",">
+                    <div className="key white c" data-note="C" data-octave="1" data-key=",">
                     </div>
-                    <div className="key black c_sharp" data-note="C#4" data-key="l">
+                    <div className="key black c_sharp" data-note="C#" data-octave="1" data-key="l">
                     </div>
-                    <div className="key white d" data-note="D4" data-key=".">
+                    <div className="key white d" data-note="D" data-octave="1" data-key=".">
                     </div>
-                    <div className="key black d_sharp" data-note="D#4" data-key=";">
+                    <div className="key black d_sharp" data-note="D#" data-octave="1" data-key=";">
                     </div>
-                    <div className="key white e" data-note="E4" data-key="/">
+                    <div className="key white e" data-note="E" data-octave="1" data-key="/">
                     </div>
-                    <div className="key white f" data-note="F4" data-key="q">
+                    <div className="key white f" data-note="F" data-octave="1" data-key="q">
                     </div>
-                    <div className="key black f_sharp" data-note="F#4" data-key="2">
+                    <div className="key black f_sharp" data-note="F#" data-octave="1" data-key="2">
                     </div>
-                    <div className="key white g" data-note="G4" data-key="w">
+                    <div className="key white g" data-note="G" data-octave="1" data-key="w">
                     </div>
-                    <div className="key black g_sharp" data-note="G#4" data-key="3">
+                    <div className="key black g_sharp" data-note="G#" data-octave="1" data-key="3">
                     </div>
-                    <div className="key white a" data-note="A4" data-key="e">
+                    <div className="key white a" data-note="A" data-octave="1" data-key="e">
                     </div>
-                    <div className="key black a_sharp" data-note="A#4" data-key="4">
+                    <div className="key black a_sharp" data-note="A#" data-octave="1" data-key="4">
                     </div>
-                    <div className="key white b" data-note="B4" data-key="r">
+                    <div className="key white b" data-note="B" data-octave="1" data-key="r">
                     </div>
-                    <div className="key white c" data-note="C5" data-key="t">
+                    <div className="key white c" data-note="C" data-octave="2" data-key="t">
                     </div>
-                    <div className="key black c_sharp" data-note="C#5" data-key="6">
+                    <div className="key black c_sharp" data-note="C#" data-octave="2" data-key="6">
                     </div>
-                    <div className="key white d" data-note="D5" data-key="y">
+                    <div className="key white d" data-note="D" data-octave="2" data-key="y">
                     </div>
-                    <div className="key black d_sharp" data-note="D#5" data-key="7">
+                    <div className="key black d_sharp" data-note="D#" data-octave="2" data-key="7">
                     </div>
-                    <div className="key white e" data-note="E5" data-key="u">
+                    <div className="key white e" data-note="E" data-octave="2" data-key="u">
                     </div>
-                    <div className="key white f" data-note="F5" data-key="i">
+                    <div className="key white f" data-note="F" data-octave="2" data-key="i">
                     </div>
-                    <div className="key black f_sharp" data-note="F#5" data-key="9">
+                    <div className="key black f_sharp" data-note="F#" data-octave="2" data-key="9">
                     </div>
-                    <div className="key white g" data-note="G5" data-key="o">
+                    <div className="key white g" data-note="G" data-octave="2" data-key="o">
                     </div>
-                    <div className="key black g_sharp" data-note="G#5" data-key="0">
+                    <div className="key black g_sharp" data-note="G#" data-octave="2" data-key="0">
                     </div>
-                    <div className="key white a" data-note="A5" data-key="p">
+                    <div className="key white a" data-note="A" data-octave="2" data-key="p">
                     </div>
-                    <div className="key black a_sharp" data-note="A#5" data-key="-">
+                    <div className="key black a_sharp" data-note="A#" data-octave="2" data-key="-">
                     </div>
-                    <div className="key white b" data-note="B5" data-key="[">
+                    <div className="key white b" data-note="B" data-octave="2" data-key="[">
                     </div>
                 </div>
             </div>
